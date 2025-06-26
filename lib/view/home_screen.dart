a8f0dab778d6145f77acf850229d5fd82bb68cc8
Aditya Sakhreliya <adityasakhareliya@gmail.com>
2025-06-26 17:13:32 +0530

    refactor: remove commented-out SongModel class from HomeScreen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -4,26 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:i_tunes/view/play_screen.dart';
 import 'package:marquee/marquee.dart';
 
-// class SongModel {
-//   final String title;
-//   final String artist;
-//   final String imageUrl;
-
-//   SongModel({
-//     required this.title,
-//     required this.artist,
-//     required this.imageUrl,
-//   });
-
-//   factory SongModel.fromJson(Map<String, dynamic> json) {
-//     return SongModel(
-//       title: json['im:name']['label'],
-//       artist: json['im:artist']['label'],
-//       imageUrl: json['im:image'][2]['label'],
-//     );
-//   }
-// }
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 

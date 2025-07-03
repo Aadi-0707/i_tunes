28443d7d5c9bd85bce53b51aff02becc3dc39521
Aditya Sakhreliya <adityasakhareliya@gmail.com>
2025-07-03 15:26:23 +0530

    feat: initialize JustAudioBackground in main function for audio playback

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:i_tunes/view/splash_screen.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+    androidNotificationIcon: 'drawable/ic_notification',
+    androidShowNotificationBadge: true,
+  );
   runApp(const MyApp());
 }
 

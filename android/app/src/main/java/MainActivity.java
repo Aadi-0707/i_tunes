151fe5de5f53727c3b957304370655035b07f494
Aditya Sakhreliya <adityasakhareliya@gmail.com>
2025-07-08 12:30:12 +0530

    commit

--- android/app/src/main/java/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.itunes; // <-- Make sure this matches your applicationId in build.gradle
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // No changes needed unless you plan to implement custom platform channels.
-}
